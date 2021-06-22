ef77818c9118d6686392a24b7f133d458b1091ca
Pola Jedrecka <pola.jedrecka5@gmail.com>
2021-06-22 11:24:17 +0200

    added items spawns

--- ObjectGenerator.py
+++ ObjectGenerator.py
@@ -22,15 +22,43 @@ def mops_character():
     , "Walkable":True, "MaxHP":100, "Armor":0, "lvlExp" : 0}
 
 def spawn_dogge(Xpoz, Ypoz):
-    inventory = [spawn_scooby_crunch(), spawn_scooby_crunch()]
+    inventory = [spawn_scooby_snack(), spawn_scooby_snack()]
     return {"Name":"dogge", "Type":"Enemy", "Symbol":"D", "Xpoz":Xpoz
     , "Ypoz":Ypoz, "Walkable":False, "Inventory":inventory
     , "HP": 100, "MaxHP":100, "BaseDamage":20, "CriticalChance":10
     , "HitChance": 60, "DodgeChance": 20, "Armor":0}
 
-def spawn_scooby_crunch():
-    return {"Name":"scooby crunch", "Type":"Consumable", "HpReward":20}
+def spawn_scooby_snack():
+    return {"Name":"Scooby snack", "Type":"Consumable", "HpReward":20}
 
 def spawn_floor():
     return {"Name":"Floor", "Type":"Map", "Symbol":".", "Walkable":True}
 
+def spawn_collar():
+    return {"Name": "Collar", "Type": "Cloth", "ArmorReward": 50}
+
+def spawn_dog_food():
+    return {"Name": "Dog food", "Type": "Consumable", "HpReward": 50}
+
+def spawn_bone():
+    return {"Name": "Bone", "Type": "Usable", "BaseDamageReward": 20, "DodgeChanceReward": -10}
+
+def spawn_stick():
+    return {"Name": "Stick", "Type": "Usable", "CriticalChanceReward": 20}
+
+def spawn_banana_peel():
+    return {"Name": "Banana peel", "Type": "Usable", "DodgeChanceReward": 0} #tu chciałam żeby całkowicie znikała możliwość uniku
+
+def spawn_ball():
+    return {"Name": "Ball", "Type": "Usable"}
+
+# Trophies
+
+def spawn_cats_claw():
+    return {"Name": "Cat's claw"}
+
+def spawn_dead_mouse():
+    return {"Name": "Dead mouse"}
+
+def spawn_boar_horn():
+    return {"Name": "Boar horn"}
